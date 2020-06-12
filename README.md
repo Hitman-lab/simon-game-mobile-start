a6d1b5cbe6c9b92ca374c93105a5f0196ad8fff1
Hitman <hackzs@outlook.com>
2020-06-12 21:24:39 +0530

    Initia commmit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simon Game can play both mobile and PC
+
+![image](https://user-images.githubusercontent.com/52199294/79674497-6a67ff00-8201-11ea-9d53-c79a3d5a2a9b.png)
+
+
+![image](https://user-images.githubusercontent.com/52199294/79674554-d480a400-8201-11ea-8963-d8d491eed84d.png)
---
+++ game.js
@@ -0,0 +1,123 @@
+let buttonColurs = ['red', 'blue', 'green', 'yellow'];
+let gamePattern = [];
+let userClickedPattern = [];
+var level = 0;
+let started = false;
+
+/* --------------- For Mobile Version ----------------- */
+
+function gameRestart() {
+    $('.game-start').add();
+};
+
+if ($(window).width() < 600) {
+  $('#level-title').text('Let\'s play!');
+  $('.game-start').add();
+} else {
+  $('.game-start').remove();
+}
+
+$('.game-start').click(function () {
+  if (!started) {
+    $('#level-title').text('Level ' + level); // Once the game has started make H1 as Level - 0
+    nextSequence();
+    started = true;
+  }
+  setTimeout(function () {
+    $('.game-start').fadeOut();
+  }, 200);
+});
+
+
+/* -------------------For Desttop Version ------------- */
+
+// Game starts here
+$(document).keypress(function () {
+  if (!started) {
+    $('#level-title').text('Level ' + level); // Once the game has started make H1 as Level - 0
+    nextSequence();
+    started = true;
+  }
+});
+
+$('.btns').click(function () {
+  // store the ID of the button tha got clicked
+  let userChosenColour = $(this).attr('id');
+  userClickedPattern.push(userChosenColour);
+  playSound(userChosenColour);
+  animationPress(userChosenColour);
+  checkAnswer(userClickedPattern.length - 1);
+});
+
+function nextSequence() {
+
+  // ready for the next level match
+  userClickedPattern = [];
+
+  level++;
+  $('#level-title').text('Level ' + level); // update H1 with this change in the value of level 
+
+  let randomNum = Math.floor(Math.random() * 4);
+  let randomChosenColour = buttonColurs[randomNum];
+  gamePattern.push(randomChosenColour);
+  $('#' + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+
+  playSound(randomChosenColour);
+};
+
+function playSound(musicName) {
+  let audio = new Audio('sounds/' + musicName + '.mp3');
+  audio.play();
+};
+
+function animationPress(currentColour) {
+  let colour = $('#' + currentColour);
+  colour.addClass('pressed');
+
+  setTimeout(function () {
+    colour.removeClass('pressed');
+  }, 50);
+};
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length) {
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+
+      if($(window).width() < 600){
+        
+        playSound('wrong');
+        $('body').addClass('game-over');
+        setTimeout(function(){
+          $('body').removeClass('game-over');
+        }, 200);
+        
+        $('#level-title').text('Game Over!');
+        $('.game-start').fadeIn();
+        startOver();
+
+      }else{
+        // console.log('wrong');
+        playSound("wrong");
+        
+        $('body').addClass("game-over");
+        
+        setTimeout(function () {
+          $('body').removeClass('game-over');
+        }, 200);
+
+        $('#level-title').text('Game Over! press any key!');
+        startOver();
+      }
+  }
+};
+
+function startOver() {
+  level = 0;
+  gamePattern = [];
+  started = false;
+};
